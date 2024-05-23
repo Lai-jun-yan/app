7000eb372435cb4e316504fdc195c8d55a36bc9a
lai <b10601043@g.ntu.edu.tw>
2024-05-23 19:23:25 +0800

    version 4

--- 特有魚app.R
+++ 特有魚app.R
@@ -3,7 +3,7 @@ library(iNEXT)
 
 # UI 程式碼
 ui <- fluidPage(
-  titlePanel("臺灣河川樣站生物多樣性分析"),
+  titlePanel("臺灣河川樣站調查"),
   
   sidebarLayout(
     sidebarPanel(
@@ -12,14 +12,18 @@ ui <- fluidPage(
       uiOutput("message"),
       uiOutput("alpha_selector"),
       uiOutput("code"),
-      uiOutput("T")
+      uiOutput("T"),
+      actionButton("start_analysis", "最佳樣站組合分析")
     ),
     
     mainPanel(
       h3("iNEXT分析結果"),
-      verbatimTextOutput("summary"),
-      plotOutput("p"),
-      verbatimTextOutput("good")
+      tabsetPanel(
+        type = "tab",
+        tabPanel("稀釋外插曲線",plotOutput("p")),
+        tabPanel("summary",tableOutput("summary")),
+        tabPanel("最佳樣站組合",tableOutput("best_sites"),verbatimTextOutput("good"))
+      )
     )
   )
 )
@@ -33,40 +37,35 @@ server <- function(input, output) {
     data <- read.csv(input$file$datapath, header = TRUE)
     
     # 過濾掉河川代碼為 "0" 和一些錯誤代碼的數據
-    data = data[data$河川代碼!="0",]
+    data = data[data$河川代碼 != "0", ]
     error_river <- c("114", "134", "242", "250")
     data <- data[!(data$河川代碼 %in% error_river), ]
     
     river <- levels(factor(data$河川代碼))
     
-    #定義 river_function
+    # 定義 river_function
     river_function = function(i){
-      
-      data_river = data[data$河川代碼 == river[i],]
+      data_river = data[data$河川代碼 == river[i], ]
       
       n_sites = length(levels(factor(data_river$locality)))
-      
       n_species = length(levels(factor(data_river$scientificName)))
       
       row_names = levels(factor(data_river$locality))
-      
       col_names = levels(factor(data_river$scientificName))
       
-      data.matrix = matrix(NA,nrow = n_sites,ncol = n_species) 
-      
+      data.matrix = matrix(NA, nrow = n_sites, ncol = n_species) 
       row.names(data.matrix) = row_names
-      
       colnames(data.matrix) = col_names
       
       for (i in 1:length(row_names)) {
         for (j in 1:length(col_names)) {
-          x = data_river[data_river$locality==row_names[i],]
-          y = x[x$scientificName==col_names[j],]
+          x = data_river[data_river$locality == row_names[i], ]
+          y = x[x$scientificName == col_names[j], ]
           z = sum(y$organismQuantity)
           if(z > 0){
-            data.matrix[row_names[i],col_names[j]] = 1
-          }else{
-            data.matrix[row_names[i],col_names[j]] = 0
+            data.matrix[row_names[i], col_names[j]] = 1
+          } else {
+            data.matrix[row_names[i], col_names[j]] = 0
           }
         }
       }
@@ -74,26 +73,23 @@ server <- function(input, output) {
       data.matrix = t(data.matrix)
       
       all_site_num = colSums(data.matrix)
-      
       able_site = which(all_site_num != 0)
+      clean_data = data.matrix[, able_site]
       
-      clean_data = data.matrix[,able_site]
+      input = c(ncol(clean_data), rowSums(clean_data))
       
-      input = c(ncol(clean_data),rowSums(clean_data))
-      
-      result = iNEXT(input,datatype = "incidence_freq")
-      
+      result = iNEXT(input, datatype = "incidence_freq")
       return(result)
-      
     }
-    
     
     results = list()
     
-    for (i in 1:length(river)) {
-      results[[i]] = river_function(i)
-      plot(river_function(i))
-    }  
+    withProgress(message = '檔案上傳中...', value = 0, {
+      for (i in 1:length(river)) {
+        results[[i]] = river_function(i)
+        incProgress(1/length(river), detail = paste("處理河川代碼:", river[i]))
+      }  
+    })
     
     # 定義 river_analyizes_function 函數
     river_analyizes_function <- function(lo) {
@@ -125,18 +121,16 @@ server <- function(input, output) {
       data.matrix <- t(data.matrix)
       
       all_site_num <- colSums(data.matrix)
-      
       able_site <- which(all_site_num != 0)
-      
       clean_data <- data.matrix[, able_site]
       
       return(clean_data)
     }
     
-    #定義site_collection
-    site_collection = function(x,t){
-      y = sample(1:ncol(x),t,replace = FALSE)
-      site_result = x[,y]
+    # 定義 site_collection
+    site_collection = function(x, t){
+      y = sample(1:ncol(x), t, replace = FALSE)
+      site_result = x[, y]
       return(site_result)
     }
     
@@ -150,7 +144,7 @@ server <- function(input, output) {
     
     # 在這裡進行任何其他後續運算
     output$message <- renderUI({
-      "已完成分析"
+      "檔案已上傳"
     })
     
     output$alpha_selector <- renderUI({
@@ -159,54 +153,61 @@ server <- function(input, output) {
       }
     })
     
-    #定義all_river_site_selection
-    all_river_site_selection = function(x,results,alpha){
-      
+    # 定義 all_river_site_selection
+    all_river_site_selection = function(x, results, alpha){
       river_1 = river_analyizes_function(x)
       
       a = results[[x]][["iNextEst"]][["coverage_based"]]
       
-      b = a[a[,4]=="Observed",2]
+      b = a[a[,4] == "Observed", 2]
       
-      c = a[,2]
+      c = a[, 2]
       
-      if(max(c) >= alpha){
-        
+      if (max(c) >= alpha){
         d = c[c >= alpha]
-        
         e = min(d)
-        
-        f = a[a[,2]==e,3]
+        f = a[a[,2] == e, 3]
         
         if(f >= ncol(river_1)){
-          cat("河川代碼:",river[x],"的總樣站數為",ncol(river_1),"，不建議減少樣站\n")
-        }else{
-          
+          river_code = river[x]
+          total_site_num = ncol(river_1)
+          advise = "不建議減少樣站"
+        } else {
           if(b < alpha){
-            cat("河川代碼:",river[x],"的總樣站數為",ncol(river_1),"，不建議減少樣站\n")
-          }else{
-            cat("河川代碼:",river[x],"的總樣站數為",ncol(river_1),"，調查",f,"個樣站時，","物種覆蓋度可達",e*100,"%\n")
+            river_code = river[x]
+            total_site_num = ncol(river_1)
+            advise = "不建議減少樣站"
+          } else {
+            river_code = river[x]
+            total_site_num = ncol(river_1)
+            advise = paste0("調查", f, "個樣站時，物種覆蓋度可達", round(e * 100,2), "%")
           }
-          
         }
-        
-      }else{
-        cat("河川代碼:",river[x],"的總樣站數為",ncol(river_1),"，不建議減少樣站\n")
+      } else {
+        river_code = river[x]
+        total_site_num = ncol(river_1)
+        advise = "不建議減少樣站"
       }
+      
+      return(c(river_code,total_site_num,advise))
       
     }
     
-    output$summary <- renderPrint({
+    output$summary <- renderTable({
       req(input$alpha)
-      cat("\n")
+      x = matrix(NA,nrow = length(river),ncol = 3)
+      colnames(x) = c("河川代碼","總樣站數","建議")
+      x = as.data.frame(x)
       for (i in 1:length(results)) {
-        cat(all_river_site_selection(i, results, as.numeric(input$alpha)), "\n")
+        y = all_river_site_selection(i,results,input$alpha)
+        x[i,1:3] = y[1:3]
       }
+      x
     })
     
     output$code <- renderUI({
       if (!is.null(input$alpha)) {
-        selectInput("c", "請選擇河川代碼:",river)
+        selectInput("c", "請選擇河川代碼:", river)
       }
     })
     
@@ -221,80 +222,87 @@ server <- function(input, output) {
     })
     
     output$T = renderUI({
-      if(!is.na(input$c)){
+      if (!is.na(input$c)){
         a <- as.numeric(input$c)
         b = which(river == a)
+        river_subset = subset(results[[b]]$iNextEst$coverage_based,Method == "Rarefaction")
         if(!is.na(b)){
-          selectInput("t","請選擇樣站數量:",results[[b]]$iNextEst$coverage_based[,3])
+          selectInput("t", "請選擇樣站數量:", river_subset[, 3])
         }
       }
     })
     
-    output$good = renderPrint({
-      if(!is.na(input$c) && !is.na(input$t)){
+    observeEvent(input$start_analysis, {
+      req(input$c, input$t)
+      output$good <- renderPrint({
         river_num = which(river == input$c)
         
-        site_selection_result = function(x,t,n = 1000000){
-          
+        site_selection_result = function(x, t, n = 1000000){
           total_site_selection = list()
-          
-          for (i in 1:n) {
-            total_site_selection[[i]] = site_collection(x,t)
-          }
-          
+          withProgress(message = '計算中...', value = 0, {
+            for (i in 1:n) {
+              total_site_selection[[i]] = site_collection(x, t)
+              if (i %% 10000 == 0) {  
+                incProgress(10000 / n, detail = paste("進度:", round(i / n * 100, 2), "%"))
+              }
+            }
+          })
           return(total_site_selection)
-          
         }
         
-        a = site_selection_result(all_river[[river_num]],input$t)
+        a = site_selection_result(all_river[[river_num]], input$t)
         
         total = sum(all_river[[river_num]])
         
         site_1 = c()
         
         for (i in 1:nrow(all_river[[river_num]])) {
-          site_1[i] = sum(all_river[[river_num]][i,])
+          site_1[i] = sum(all_river[[river_num]][i, ])
         }
         
-        true_ratio = site_1/total
+        true_ratio = site_1 / total
         
-        bias_function = function(x,true_ratio){
-          
+        bias_function = function(x, true_ratio){
           total = sum(x)
           
           site_1 = c()
           
           for (i in 1:nrow(x)) {
-            site_1[i] = sum(x[i,])
+            site_1[i] = sum(x[i, ])
           }
           
-          ratio = site_1/total
+          ratio = site_1 / total
           
           bias = mean(abs(true_ratio - ratio))
           
           return(bias)
-          
         }
         
         k = c()
         
         for (i in 1:length(a)) {
-          k[i] = bias_function(a[[i]],true_ratio)
+          k[i] = bias_function(a[[i]], true_ratio)
         }
         
-        w = which(results[[b]]$iNextEst$coverage_based[,3]==input$t)
+        w = which(results[[river_num]]$iNextEst$coverage_based[, 3] == input$t)
         
-        good_site = paste0("以上為最佳樣站組合，物種組成偏誤值:",min(k)*100,"%")
+        n = as.numeric(results[[river_num]]$iNextEst$coverage_based[w, 6])
         
-        species_num = paste0("預測物種數為:",results[[b]]$iNextEst$coverage_based[w,6])
+        co = results[[river_num]]$iNextEst$coverage_based$t
         
-        list(colnames(a[[which.min(k)]]),c(good_site,species_num))
-      }
+        best_sites <- colnames(a[[which.min(k)]])
+        
+        output$best_sites <- renderTable({
+          data.frame("最佳樣站" = best_sites) 
+        })
+        
+        cat("以上為最佳樣站組合，物種組成差異:",round(min(k) * 100,2), "%，","物種數:", round(n),"，","物種覆蓋度:",round(results[[river_num]]$iNextEst$coverage_based[co == input$t,2]*100,2),"%","。")
+        
+      })
     })
-    
   })
-  
 }
 
 # 啟動應用
 shinyApp(ui = ui, server = server)
+
